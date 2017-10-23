58f449e31b388bf30722306d9dadddea6b0dee50
z0z <472354643@qq.com>
2017-10-23 21:45:35 +0800

    add yzm.py

---
+++ misc/yzm.py
@@ -0,0 +1,24 @@
+#-*-coding:utf-8-*-
+
+import pytesseract
+from PIL import Image
+import requests
+import os
+
+def vcode():
+    pic_url = 'http://xxx/vcode.php'
+    r = requests.get(pic_url)
+    with open('tmp.png', 'wb') as pic:
+        pic.write(r.content)
+    try:
+        im = pytesseract.image_to_string(Image.open('tmp.png'))
+        im = im.replace(' ', '')
+        '''
+        if im != '':
+            return im
+        else:'''
+        return vcode()
+    except:
+        return 0
+
+print vcode()
